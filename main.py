697417772eea455db7fad42671bb392254dc9030
IT STEP <memasik4you@gmal.com>
2023-10-03 19:04:14 +0300

    ex2

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+def odd_numbers_between(a, b):
+    for number in range(a + 1, b):
+        if number % 2 != 0:
+            print(number)
 
-def format_text():
-    text ='''"Don't let the noise of others' opinions
-drown out your own inner voice."
-Steve Jobs '''
-    print(text)
-
-format_text()
-
+odd_numbers_between(1, 10)
 
